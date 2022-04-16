ecbc0e9fd55c2c56b67424bd4bf089ad2f42b030
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-04-16 13:48:37 +0430

    not sending value of hashlength fixed

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,5 +1,5 @@
 function formatUrl(path) {
-  const adjustedPath = path[0] !== '/' ? `/${path}` : path;
+  const adjustedPath = path[0] !== "/" ? `/${path}` : path;
   return process.env.REACT_APP_BASE_URL + adjustedPath;
   // return adjustedPath;
 }
@@ -13,10 +13,10 @@ function checkStatus(response) {
 
 async function parseJSON(response) {
   if (response && response.headers) {
-    if (response.headers.get('Content-Type') === 'application/json') {
+    if (response.headers.get("Content-Type") === "application/json") {
       return await response.json();
     }
-    if (response.headers.get('Content-Type') === 'text/plain;charset=UTF-8') {
+    if (response.headers.get("Content-Type") === "text/plain;charset=UTF-8") {
       return await response.text();
     }
   }
@@ -28,25 +28,25 @@ function ApiClient(path, options) {
   const fetchOptions = options;
   fetchOptions.headers = fetchOptions.headers || {};
 
-  if (fetchOptions.type === 'formdata') {
+  if (fetchOptions.type === "formdata") {
     fetchOptions.body = new FormData();
 
     for (let key in options.data) {
       if (
-        typeof key === 'string' &&
+        typeof key === "string" &&
         options.data.hasOwnProperty(key) &&
-        typeof options.data[key] !== 'undefined'
+        typeof options.data[key] !== "undefined"
       ) {
         fetchOptions.body.append(key, options.data[key]);
       }
     }
   } else {
     fetchOptions.body = JSON.stringify(options.data);
-    fetchOptions.headers['Content-Type'] = 'application/json';
-    fetchOptions.headers['Accept'] = 'application/json';
+    fetchOptions.headers["Content-Type"] = "application/json";
+    fetchOptions.headers["Accept"] = "application/json";
   }
 
-  const user = JSON.parse(window.localStorage.getItem('user'));
+  const user = JSON.parse(window.localStorage.getItem("user"));
   if (user && user.token) {
     fetchOptions.headers.Authorization = `Bearer ${user.token}`;
   }
