cd51d908744ee536673efa8688d77957060c71c1
Behzad <saeedibehzad@gmail.com>
2022-07-27 12:15:44 +0430

    using pie chart of HightChart

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { Col, Row } from 'antd'
 import FiveTopIp from '../../components/fiveTopIp'
 import AgentNames from '../../components/agentNames'
-import Circle from '../../components/agentNames/Circle'
 
 const Dashboard = () => {
   return (
-    <Row style={{ padding: 16 }} gutter={20}>
-      <Col span={4}>
-        <FiveTopIp />
-      </Col>
-      <Col span={10} >
-        <AgentNames />
-      </Col>
-    </Row>
+    <div style={{ backgroundColor: '#fafafa', padding: 16, height: '100%' }}>
+      <Row gutter={20}>
+        <Col span={4}>
+          <FiveTopIp />
+        </Col>
+        <Col span={8}>
+          <AgentNames />
+        </Col>
+      </Row>
+    </div>
   )
 }
 
