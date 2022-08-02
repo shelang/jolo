b1c25bbec7c8a2e258cf17f093932fe1b30f312a
Behzad <saeedibehzad@gmail.com>
2022-08-02 19:28:20 +0430

    seprate operationSystemTargeting

--- src/containers/createLink/content/CreateLinkForm.js
+++ src/containers/createLink/content/CreateLinkForm.js
@@ -33,6 +33,7 @@ import WebhookSection from '../../../components/createLinkWebhook'
 import { SuccefullModal } from './succesfullModal'
 import { reorderObjectKeys } from '../../../utils/general/reorderObjectKeys'
 import { deleteObjectKey } from '../../../utils/general/deleteObjectKey'
+import { OperationSystemTargeting } from './OperationSystemTargeting'
 
 const { TextArea } = Input
 const { Title } = Typography
@@ -353,103 +354,12 @@ const CreateLinkForm = () => {
                   onSetTargetDevices={setTargetDevices}
                   Form={Form}
                 />
-                <Card>
-                  <Title level={3}>
-                    Operation System Targeting:
-                    <Tooltip
-                      className={'customTooltip'}
-                      placement="top"
-                      title={tooltips.textTargeting}>
-                      <Button>?</Button>
-                    </Tooltip>
-                  </Title>
-                  <Form.List name="os">
-                    {(fields, { add, remove }) => (
-                      <>
-                        {fields.map(
-                          ({
-                            key,
-                            name,
-                            fieldKey,
-                            labelCol,
-                            wrapperCol,
-                            ...restField
-                          }) => (
-                            <Space
-                              key={key}
-                              size={0}
-                              style={{ display: 'flex', marginBottom: 8 }}
-                              align="baseline">
-                              <Form.Item
-                                {...restField}
-                                wrapperCol={{ span: 24 }}
-                                name={[name, 'type']}
-                                fieldKey={[fieldKey, 'type']}
-                                rules={[
-                                  { required: true, message: 'Missing type' },
-                                ]}>
-                                <Select
-                                  style={{ width: 200 }}
-                                  placeholder="Operation System">
-                                  {operationSystems.map((operationSystem) => {
-                                    if (
-                                      Object.values(selectedOs).includes(
-                                        operationSystem.toLowerCase(),
-                                      )
-                                    ) {
-                                      return null
-                                    } else {
-                                      return (
-                                        <Select.Option
-                                          value={operationSystem.toLowerCase()}>
-                                          {operationSystem}
-                                        </Select.Option>
-                                      )
-                                    }
-                                  })}
-                                </Select>
-                              </Form.Item>
-                              <Form.Item
-                                {...restField}
-                                wrapperCol={{ span: 24 }}
-                                name={[name, 'url']}
-                                fieldKey={[fieldKey, 'url']}
-                                rules={[
-                                  { required: true, message: 'Missing URL' },
-                                ]}>
-                                <Input
-                                  placeholder="url"
-                                  style={{ width: 300 }}
-                                />
-                              </Form.Item>
-                              <MinusCircleOutlined
-                                style={{ marginLeft: 10 }}
-                                onClick={() => {
-                                  const newSelectedOs = reorderObjectKeys(
-                                    deleteObjectKey(selectedOs, name),
-                                  )
-                                  setSelectedOs(newSelectedOs)
-                                  remove(name)
-                                }}
-                              />
-                            </Space>
-                          ),
-                        )}
-                        <Form.Item>
-                          {fields.length < 2 && (
-                            <Button
-                              type="dashed"
-                              onClick={() => add()}
-                              block
-                              icon={<PlusOutlined />}>
-                              Add Operation System
-                            </Button>
-                          )}
-                        </Form.Item>
-                      </>
-                    )}
-                  </Form.List>
-                </Card>
+                <OperationSystemTargeting
+                  Form={Form}
+                  onOperationSystems={operationSystems}
+                  onSelectedOs={selectedOs}
+                  onSetSelectedOs={setSelectedOs}
+                />
               </Space>
             </Col>
           </Row>
